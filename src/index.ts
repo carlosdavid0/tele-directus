47952799bb7fe01d9cfa7d0b7b318472447f7c1b
Carlos David <romao.carlosdavi@gmail.com>
2024-01-19 07:54:50 -0300

    :sparkles: get price, free  shipping and old price

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,102 @@
+import axios from 'axios';
 import { Telegraf, Markup } from 'telegraf';
 require('dotenv').config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN as string);
-
-import axios from 'axios';
+import { load } from 'cheerio';
 import puppeteer from 'puppeteer';
 
-import { load } from 'cheerio'
+try {
 
-bot.start((ctx) => {
-    let message = `Please select an option from the menu below`;
-    ctx.replyWithHTML(message, Markup.keyboard([
-        ['Option 1', 'Option 2'], // Linha 1 com 2 opções
-        ['Option 3', 'Option 4'], // Linha 2 com 2 opções
-        ['Option 5', 'Option 6', 'Option 7'] // Linha 3 com 3 opções
-    ]).resize());
-});
+    const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-bot.hears('Option 1', async (ctx) => {
-
-   
-
-});
-
-
-bot.on('text', async (ctx) => {
-    const url = ctx.message.text;
-
-    // verify if this is a url valid
-    const regex = new RegExp('^(ftp|http|https):\/\/[^ "]+$');
-
-    if (!regex.test(url)) {
-        ctx.reply('Invalid URL');
-        return;
-    }
-    
-
-    axios.get(url).then(response => {
-
-        const html = load(response.data)
-
-        const productName = html('script[type="application/ld+json"]').first().html();
-
-        let parsedJson = JSON.parse(productName as string);
-
-
-        const {name, image, offers} = parsedJson
-        
-       
-        ctx.reply({text: name})
-       
-        ctx.reply({text: offers.price})
-        ctx.sendPhoto(image)
-        // tolocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-
+    bot.start((ctx) => {
+        let message = `Please select an option from the menu below`;
+        ctx.replyWithHTML(message, Markup.keyboard([
+            ['Option 1', 'Option 2'], // Linha 1 com 2 opções
+            ['Option 3', 'Option 4'], // Linha 2 com 2 opções
+            ['Option 5', 'Option 6', 'Option 7'] // Linha 3 com 3 opções
+        ]).resize());
     })
-})
 
-bot.launch();
+    bot.hears('Option 1', async (ctx) => {
+        const url = ctx.message.text;
+
+        // verify if this is a url valid
+        const regex = new RegExp('^(ftp|http|https):\/\/[^ "]+$');
+
+        if (!regex.test(url)) {
+            ctx.reply('Invalid URL');
+            return;
+        }
+
+        axios.get(url).then(response => {
+            const html = load(response.data)
+            const productName = html('script[type="application/ld+json"]').first().html();
+            let parsedJson = JSON.parse(productName as string);
+            const { name, image, offers } = parsedJson
+            ctx.reply({ text: name })
+            ctx.reply({ text: offers.price })
+            ctx.sendPhoto(image)
+        })
+    });
+
+    bot.on('text', async (ctx) => {
+        const url = ctx.message.text;
+
+        // verify if this is a url valid
+        const regex = new RegExp('^(ftp|http|https):\/\/[^ "]+$');
+
+        if (!regex.test(url)) {
+            ctx.reply('Invalid URL');
+            return;
+        }
+
+        axios.get(url).then(response => {
+            const html = load(response.data)
+            const productName = html('script[type="application/ld+json"]').first().html();
+
+            const oldPrice = html('span.andes-money-amount__fraction').first().html();
+
+            let parsedJson = JSON.parse(productName as string);
+
+
+            console.log(parsedJson);
+            
+
+            const { name, image, offers } = parsedJson
+            ctx.reply({ text: name })
+            ctx.reply({ text: offers.price })
+            ctx.sendPhoto(image)
+
+            puppeteer.launch().then(async browser => {
+
+                // Open new page
+                const page = await browser.newPage();
+
+                // Go to website
+                await page.goto(url);
+
+                await page.screenshot({ path: 'example.png', fullPage: true });
+            })
+
+            const freeShippingElement = html('.ui-pdp-color--GREEN');
+
+            if (freeShippingElement.text().toUpperCase().includes('FRETE GRÁTIS')) {
+                ctx.reply('Free shipping!');
+            } else {
+                ctx.reply('Not free shipping.');
+            }
+        })
+    });
+
+
+    bot.launch().then(() => {
+        console.log('Bot started');
+    }
+    ).catch((err) => {
+        console.log(err);
+    });
+
+} catch (error) {
+    console.log(error);
+}
