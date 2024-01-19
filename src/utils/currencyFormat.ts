8ab632b168d68a2acccb41a4b59b08e2ca9619a9
Carlos David <romao.carlosdavi@gmail.com>
2024-01-19 16:58:07 -0300

    :sparkles: formatadores de moeda

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,28 @@
+export function toReal(number: number): string {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(number);
+  }
+  
+  export function parsePrice(priceString: string): string {
+   let priceFloat: number = parseFloat(priceString.replace('R$ ', '').replace('.', '').replace(',', '.'));
+   return toReal(priceFloat);
+  }
+
+
+ export function formatarMoedaBRL(valor: number | string): string {
+
+    
+    const valorNumerico = typeof valor === 'string' ? parseFloat(valor.replace('', '.')) : valor;
+    
+    console.log(valorNumerico);
+
+    if (isNaN(valorNumerico)) {
+        throw new Error('Valor não é um número válido.');
+    }
+
+    const formatoMoeda = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+    return formatoMoeda.format(valorNumerico);
+}
